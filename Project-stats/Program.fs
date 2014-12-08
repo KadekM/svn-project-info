fafb601ccce7985ce653de9e69916515a9320c8b
marek.kadek <marek.kadek@erni.sk>
2014-12-08 14:55:03 +0100

    Initial commit

---
+++ Project-stats/Program.fs
@@ -0,0 +1,19 @@
+﻿open FSharp.Data;
+
+type LogProvider = XmlProvider<"sample-log.xml">
+let provider path = LogProvider.Parse(path) 
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    let log = LogProvider.Load "log.xml"
+
+    let commitsFrom author = log.Logentries |> Seq.where (fun x -> x.Author = author)
+
+    let authors = [| "marek.kadek"; "martin.kolinek"; "vladimir.pavelka"; "marek.sedlacek"; "robert.herceg"; "branislav.pavelka" |]
+
+    authors |> Seq.map (fun x -> (x, commitsFrom x |> Seq.length ) ) |> Seq.iter (fun (author, commits) -> printfn "%A : %A" author commits)
+    System.Console.ReadKey() |> ignore
+    0
