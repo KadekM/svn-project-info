a6e81e948686a11af01de35136116b98a361de09
marek.kadek <marek.kadek@erni.sk>
2014-12-08 16:59:11 +0100

    Abstracted parameter away

--- Project-stats/Program.fs
+++ Project-stats/Program.fs
@@ -7,9 +7,9 @@ let provider path = LogProvider.Parse(path)
 [<EntryPoint>]
 let main argv = 
     let path = System.Environment.GetEnvironmentVariable("PATH").Split(';') |> Seq.map(fun x -> System.IO.Path.Combine(x, "svn.exe")) |> Seq.where System.IO.File.Exists|> Seq.head
-
+    let repo = argv.[0]
     let procInfo = System.Diagnostics.ProcessStartInfo(path)
-    procInfo.Arguments <- "log --xml"
+    procInfo.Arguments <- "log " + repo + " --xml"
     procInfo.WorkingDirectory <- System.Environment.CurrentDirectory
     procInfo.RedirectStandardOutput <- true
     procInfo.CreateNoWindow <- true
@@ -22,9 +22,9 @@ let main argv =
     let log = LogProvider.Parse(proc.StandardOutput.ReadToEnd())
     
     let commitsFrom author = log.Logentries |> Seq.where (fun x -> x.Author = author)
-
+    
     let authors = [| "marek.kadek"; "martin.kolinek"; "vladimir.pavelka"; "marek.sedlacek"; "robert.herceg"; "branislav.pavelka" |]
 
-    authors |> Seq.map (fun x -> (x, commitsFrom x |> Seq.length ) ) |> Seq.iter (fun (author, commits) -> printfn "%A : %A" author commits)
+    authors |> Seq.map (fun x -> (x, commitsFrom x |> Seq.length ) ) |> Seq.sortBy(fun (_, amount) -> amount) |> Seq.iter (fun (author, commits) -> printfn "%A : %A" author commits)
     System.Console.ReadKey() |> ignore
     0
